e98f2111de3fff56649928fa6ba3e807ca15c1b6
Erik Lim <elim@stevens.edu>
2018-08-26 21:57:06 -0400

    Add files via upload

---
+++ HW 00 - Tools Setup.py
@@ -0,0 +1,9 @@
+'''
+Created on August 26th, 2018
+@author:  Erik Lim
+Pledge:   "I pledge my honor that I have abided by the Stevens Honor System." - EL
+
+Homework 00 - Tools Setup
+'''
+
+print ("Hello world")
